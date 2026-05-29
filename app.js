2e5b2f9188c158a7b820037911f60ecd01862ccb
jorgevargas1590-commits <jorge.vargas.1590@gmail.com>
2026-05-29 13:43:34 -0400

    feat: version inicial vulnerable a XSS

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  const name = req.query.name || 'World';
+  // Vulnerable a XSS Reflejado
+  res.send(`<h1>Hello, ${name}!</h1>`);
+});
+
+app.listen(port, () => {
+  console.log(`App listening on http://localhost:${port}`);
+});
